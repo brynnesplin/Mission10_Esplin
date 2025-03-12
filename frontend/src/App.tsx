a1f0004ec3282c8078f258fd4e6342df63f7fb87
Brynn Esplin <besplin1@byu.edu>
2025-03-12 17:14:19 -0600

    Heading

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,23 @@
 import './App.css'
 import BowlerList from './bowlerList'
 
+function Heading(){
+  return(
+    <>
+
+    <h1>Crazy Bowlers</h1>
+    <p>These are all the bowlers on the Marlins and the Sharks</p>
+
+    </>
+  )
+
+}
+
 function App() {
 
   return (
     <>
+      <Heading/>
       <BowlerList/>
     </>
   )
